229cbdfcd061662019c54e5aefc95f1ab2f9dd09
luciano <luxtucu@gmail.com>
2021-03-02 00:40:35 -0300

    firebase 3

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -26,7 +26,8 @@ const ItemCount = ({stock, setStock, initial, setInitial, cart, setCart, product
 
         const item = {
             item: {
-                id: product.productId,
+                id: product.id,
+                productId: product.productId,
                 title: product.title,
                 price: product.price,
                 image: product.image
--- src/containers/CartContainer/CartContainer.js
+++ src/containers/CartContainer/CartContainer.js
@@ -1,11 +1,75 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
+import { getFirestore } from '../../firebase'
+import firebase from 'firebase'
 import './CartContainer.css'
 
 export const CartContainer = () => {
 
+    const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [orderId, setOrderId] = useState('')
+
     const {cart, removeItem, clearCart} = useContext(CartContext)
+
+    const createOrder = async (e) => {
+        e.preventDefault()
+
+        let buyer = {
+            name: name,
+            lastName: lastName,
+            phone: phone
+        } 
+
+        let total = cart.reduce((a,c) => a + c.qty * c.item.price, 0)
+
+        let order = {
+            buyer: buyer,
+            items: [...cart],
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: total
+        }
+
+        const fsDB = getFirestore()
+        const ordenesCollection = fsDB.collection('orders')
+
+        ordenesCollection
+            .add(order)
+            .then(value => {
+                setOrderId(value.id)
+                clearCart()
+            })
+            .catch(error => {
+                console.log(error)
+            })  
+
+        updateStock(order)
+
+        setName('')
+        setLastName('')
+        setPhone('')
+    }
+
+    const updateStock = (order) => {
+        order.items.map((elemento, indice) => {
+            let id = elemento.item.id
+            let amount = elemento.qty
+
+            var itemsCollection = getFirestore().collection('items')
+            
+            var docRef = itemsCollection.doc(id)
+            docRef.get().then(value => {
+                var stockPrevio = value.data().stockItem
+
+                return docRef.update({
+                    stockItem: stockPrevio - amount
+                })
+            })
+            
+        })
+    }
 
     useEffect(() => {
 
@@ -53,8 +117,44 @@ export const CartContainer = () => {
             <div className='cartContainer__price'>
                 <div className='cartContainer__priceInfo'>
                     <h2>Subtotal ({cart.reduce((a,c) => a + 1 * c.qty, 0)} items) : $ {cart.reduce((a,c) => a + c.qty * c.item.price, 0)} </h2>
-                    <button>Realizar Compra</button>
-                    <div className='cartContainer__clear'><button onClick={() => clearCart()}>Limpiar carrito</button></div>
+
+                </div>
+
+                <div className='cartContainer__priceInfo'>
+
+                    {!orderId ? (
+                    <form className='cartContainer__userInfo' onSubmit={(e) => createOrder(e)}>
+                        <h3>Informacion Usuario</h3>
+                        <div>
+                            <input type='text' placeholder='Nombre...' value={name} onChange={e => setName(e.target.value)} required></input>
+                        </div>
+
+                        <div>
+                            <input type='text' placeholder='Apellido...' value={lastName} onChange={e => setLastName(e.target.value)} required></input>
+                        </div>
+
+                        <div>
+                            <input type='text' placeholder='Telefono...' value={phone} onChange={e => setPhone(e.target.value)} required></input>
+                        </div>
+
+                        <div>
+                            <button type='submit'>Realizar Compra</button>
+                        </div>
+                    </form>
+                    ) : (
+                        <div className='cartContainer__confirm'>
+                            <h2>Su compra se ha concretado</h2>
+                            <p>Comprobante N°: {orderId}</p>
+                        </div>
+                    )}
+
+                    <div className='cartContainer__clear'>
+                        {!orderId ?
+                        <button className='cartContainer__clearButton' onClick={() => clearCart()}>Limpiar carrito</button> :
+                        <Link to='/'><button className='cartContainer__continueButton' style={{backgroundColor: 'green'}}>Seguir Comprando</button></Link>
+                        }
+                    </div>
+
                 </div>
 
             </div>
